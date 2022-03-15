4a2e56fcba8a3b2b336f275d4fa2a22dee651634
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-14 23:24:17 -0300

    feat: componente

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component} from "react";
+import TextCounter from "./TextCounter";
+function App() {
+  return (
+    <div >
+        <TextCounter />
+    </div>
+  );
+}
+
+export default App;
