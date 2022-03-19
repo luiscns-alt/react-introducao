0fd5fe330a8e78c91ee26bb538ca5f46cafc96bd
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-19 16:53:32 -0300

    componentes não controlados

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,15 @@ import React, {Component} from "react";
 
 import TextCounter from "./TextCounter";
 import MyForm from "./MyForm";
+import MyForm2 from "./MyForm2";
 
 function App() {
   return (
       <div>
         <TextCounter limit={20} />
           <MyForm />
+          <hr />
+          <MyForm2 />
       </div>
   );
 }
---
+++ my-app/src/MyForm2.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class MyForm2 extends Component {
+    constructor(props) {
+        super(props);
+
+        this.inputName = React.createRef();
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        console.log(this.inputName.current.value);
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label>
+                        Name:
+                        <input
+                            type='text'
+                            name='name'
+                            ref={this.inputName}
+                        />
+                    </label>
+                </div>
+                <input type="submit" value='Enviar' />
+            </form>
+        );
+    }
+}
+
+export default MyForm2;
