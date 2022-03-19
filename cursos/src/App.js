d720c2d9623c7d536bea4a8ad61ded121aacbb84
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-19 19:27:03 -0300

    projeto: criando formulários

--- cursos/src/App.js
+++ cursos/src/App.js
@@ -1,44 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Course from './components/Course';
+import NewCourseForm from "./components/NewCourseForm";
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            courses: [
-                {
-                    id: 1,
-                    name: 'React',
-                    category: 'Javascript',
-                    image:
-                        'https://pbs.twimg.com/card_img/1501118865707012096/SvDbtNrl?format=png&name=medium',
-                },
-                {
-                    id: 2,
-                    name: 'Angular',
-                    category: 'Javascript',
-                    image:
-                        'https://static.imasters.com.br/wp-content/uploads/2021/05/07095345/angular.png',
-                },
-            ],
+            courses: [],
         };
+        this.add = this.add.bind(this);
         this.remove = this.remove.bind(this);
     }
 
+    add(course) {
+        const { courses } = this.state,
+            NewCourse = Object.assign({}, course, { id: (Date.now()) })
+        courses.push(course);
+        this.setState({ courses });
+    }
+
     remove(courseId) {
-        const {courses} = this.state,
+        const { courses } = this.state,
             courseIndex = courses.findIndex((course) => course.id === courseId);
 
         courses.splice(courseIndex, 1);
-        this.setState({courses});
+        this.setState({ courses });
     }
 
     render() {
-        const {state} = this;
+        const { state } = this;
         return (
             <div className='App'>
+                <NewCourseForm onSubmit={this.add} />
                 <ul className='courses-list'>
                     {state.courses.map((course) => (
                         <Course course={course} onRemove={this.remove} />
